2e7204d21af1c0e636a65ef24305f2cd0364881a
Colby M. White <colbywhite@users.noreply.github.com>
2023-06-08 12:56:47 -0500

    feat: add dummy generator script

---
+++ scripts/generateForm.ts
@@ -0,0 +1,7 @@
+// To access your database
+// Append api/* to import from api and web/* to import from web
+import { db } from 'api/src/lib/db'
+
+export default async ({ args }) => {
+  console.warn('Not implemented yet')
+}
