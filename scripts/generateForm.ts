d75ec3b90ef684e4cb126c5f1297fddd744fec3a
Colby M. White <colbywhite@users.noreply.github.com>
2023-06-09 12:18:40 -0500

    refactor: use conventions from RW's cli in script

--- scripts/generateForm.ts
+++ scripts/generateForm.ts
@@ -1,7 +1,24 @@
 // To access your database
 // Append api/* to import from api and web/* to import from web
-import { db } from 'api/src/lib/db'
+// import { db } from 'api/src/lib/db'
+import { Listr } from 'listr2'
 
-export default async ({ args }) => {
-  console.warn('Not implemented yet')
+import { colors } from '@redwoodjs/cli-helpers'
+import { errorTelemetry } from '@redwoodjs/telemetry'
+
+export default async ({ args: scriptArgs }) => {
+  const tasks = new Listr({
+    title: 'Generate form',
+    task: async (taskArgs) => {
+      console.log({ taskArgs, scriptArgs })
+      throw new Error('generateForm script is not implemented yet')
+    },
+  })
+  try {
+    await tasks.run()
+  } catch (e) {
+    errorTelemetry(process.argv, e.message)
+    console.error(colors.error(e.message))
+    process.exit(e?.exitCode || 1)
+  }
 }
